bb7cafcbd5bc249f792b790d95263e5d337432ed
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-08-31 12:26:06 +0600

    Weather Icon and Text JS Added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-//Key 
+//****************************************************************************
+//            Async Function For Loading City Weather Data
 async function loadCityWeather() {
     const searchCityInput = document.getElementById('search-input');
     const searchCity = searchCityInput.value;
@@ -8,23 +9,35 @@ async function loadCityWeather() {
         const res = await fetch(url);
         const data = await res.json();
         displayWeatherData(data);
+        console.log(data);
     }
     catch (error) {
         console.log(error);
     }
 }
 
+
+//****************************************************************************
+//                  Display Weather Data to HTML
 function displayWeatherData(weather) {
-    const displayResult = document.getElementById('display-result');
+    const displayResult = document.getElementById('display-result');    
     displayResult.innerHTML =
         `
         <h3 class="text-white text-center mt-4">${weather.name}</h3>
-        <h4 class="text-white text-center mt-2">${FahrenheitTOCentigrade(weather.main.temp)}</h4>
-        <img src="" alt="">`;
+        <h1 class="text-white text-center mt-2">${KelvinTOCentigrade(weather.main.temp)}&#176;</h1>
+        <div class="weather-icon-div mt-3">
+           <div>
+                <img class="weather-icon" src="https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png" alt="">
+                <p>${weather.weather[0].main}</p>
+           </div>
+        </div>`;
 }
 
-const FahrenheitTOCentigrade = temperature => {
-    let fTemp = parseFloat(temperature);
-    let fToCel = (fTemp -273.15);   
-    return fToCel.toFixed(2);
+
+//****************************************************************************
+//                      Convert Kelvin to Cel
+const KelvinTOCentigrade = temperature => {
+    let kelvin = parseFloat(temperature);
+    let centigrade = (kelvin -273.15);   
+    return centigrade.toFixed(2);
 }
